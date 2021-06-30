d490a2d8a0258b1bf06a944c2fe8ba62927e8aa1
peter <petrel.harp@gmail.com>
2021-06-30 10:15:18 -0700

    added birkner refs

--- sims/Rsims/run_sim.R
+++ sims/Rsims/run_sim.R
@@ -1,15 +1,19 @@
 source("patch_functions.R")
 library(Matrix)
+library(matrixStats)
+library(jsonlite)
 
 pfile <- commandArgs(TRUE)[1]
 outdir <- gsub("\\.R$", "", pfile)
 dir.create(outdir, showWarnings=FALSE, recursive=TRUE)
 
-penv <- environment()
+penv <- new.env()
 source(pfile, local=penv)
 params <- as.list(penv)
 params$seed <- floor(1e7 * runif(1))
 outbase <- sprintf("%s/%d", outdir, params$seed)
+outfile <- file(paste0(outbase, "_vars.csv"), open='w')
+writeLines(paste("#", jsonlite::toJSON(params)), con=outfile)
 
 # params <- list(
 #     PGOOD = 0.1,  # mean length of a patch is 1/PGOOD
@@ -20,6 +24,7 @@ outbase <- sprintf("%s/%d", outdir, params$seed)
 #     seed = 123
 # )
 
+
 set_params(params)
 set.seed(params$seed)
 
@@ -28,11 +33,15 @@ mu <- matrix(0, nrow=length(starts), ncol=params$NGRID)
 for (k in seq_along(starts)) {
     mu[k, starts[k]] <- 1.0
 }
-dL2 <- outer(1:params$NGRID, starts, "-")^2
+# min( x-y, y + n - x )
+dL2 <- pmin(outer(1:params$NGRID, starts, "-")^2,
+            outer(1:params$NGRID, params$NGRID + starts, "-")^2,
+            outer(params$NGRID + 1:params$NGRID, starts, "-")^2
+           )
 
 num_snapshots <- 101
 out <- data.frame(
-    times = params$BURNIN + unique(floor(seq(1, params$NUMGENS, length.out=num_snapshots)))
+    time = params$BURNIN + unique(floor(seq(1, params$NUMGENS, length.out=num_snapshots)))
 )
 out$prop_good <- NA
 out$num_patches <- NA
@@ -42,19 +51,22 @@ varmat <- matrix(NA, nrow=num_snapshots, ncol=length(starts))
 colnames(varmat) <- paste0("rep_", 1:ncol(varmat))
 
 if (params$plotit) {
-    landscape <- matrix(NA, nrow=length(out$times), ncol=params$NGRID)
-    mumat1 <- mumat2 <- matrix(NA, nrow=length(out$times), ncol=params$NGRID)
+    landscape <- matrix(NA, nrow=length(out$time), ncol=params$NGRID)
+    mumat1 <- mumat2 <- matrix(NA, nrow=length(out$time), ncol=params$NGRID)
     mumat1[1,] <- as.vector(mu[1,])
     mumat2[1,] <- as.vector(mu[length(starts),])
 }
+
+header <- paste(sprintf('"%s"', c(colnames(out), colnames(varmat))), collapse=',')
+writeLines(header, con=outfile)
 
 now <- InitLandscape(params$NGRID);
 for (gen in 2:(params$BURNIN+params$NUMGENS)) {
   now <- UpdateLandscape(now, debug=FALSE);
   if (gen > params$BURNIN) {
       mu <- update_dist(mu, now)
-      if (gen %in% out$times) {
-          idx <- match(gen, out$times)
+      if (gen %in% out$time) {
+          idx <- match(gen, out$time)
           out$prop_good[idx] <- mean(now)
           plens <- patch_lengths(now)
           out$num_patches[idx] <- length(plens)
@@ -63,6 +75,8 @@ for (gen in 2:(params$BURNIN+params$NUMGENS)) {
           for (k in seq_along(starts)) {
               varmat[idx, k] <- sum(mu[k,] * dL2[,k,drop=FALSE])
           }
+          outrow <- c(out[idx,], varmat[idx,])
+          writeLines(paste(outrow, collapse=','), con=outfile)
           if (params$plotit) {
               landscape[idx,] <- now
               mumat1[idx,] <- as.vector(mu[1,])
@@ -72,22 +86,28 @@ for (gen in 2:(params$BURNIN+params$NUMGENS)) {
   }
 }
 
+plot_mumat <- function (mm) {
+    image(out$time, 1:ncol(mm), mm, xlab='time', ylab='space')
+    mF <- rowCumsums(mm)
+    qq <- do.call(cbind, lapply(c(0.025, 0.25, 0.75, 0.975), function (q) {
+                    apply(mF, 1, function (x) min(which(x>=q)))
+        } ) )
+    matlines(out$time, qq, type='l', lty=c(2,1,1,2), col='grey')
+}
+
 if (params$plotit) {
     png(file=paste0(outbase, "_landscape.png"), width=200*5, height=200*8, res=200)
     layout(matrix(1:6, nrow=3))
     image(landscape)
-    image(mumat1)
-    image(mumat2)
-    matplot(out$times, varmat/1:nrow(varmat), type='l', lty=1, xlab='time', ylab='variance/time')
-    plot(out$times, out$prop_good, xlab='time', ylab='proportion good', type='l')
-    abline(h=PBAD/(PBAD+PGOOD), col='red')
-    plot(out$times, out$num_patches / (NGRID/(1/PGOOD+1/PBAD)), xlab='time', ylab='scaled expectation', type='l')
-    lines(out$times, out$mean_length / (1/PGOOD), xlab='time')
+    plot_mumat(mumat1)
+    plot_mumat(mumat2)
+    with(penv,  {
+        matplot(out$time, varmat/out$time, type='l', lty=1, xlab='time', ylab='variance/time')
+        abline(h=2/3, col='red')
+        plot(out$time, out$prop_good, xlab='time', ylab='proportion good', type='l')
+        abline(h=PBAD/(PBAD+PGOOD), col='red')
+        plot(out$time, out$num_patches / (NGRID/(1/PGOOD+1/PBAD)), xlab='time', ylab='scaled expectation', type='l')
+        lines(out$time, out$mean_length / (1/PGOOD), xlab='time', col='green')
+    } )
     abline(h=1, col='red')
 }
-
-write.csv(
-        cbind(out, varmat),
-        file=paste0(outbase, "_vars.csv"),
-        row.names=FALSE
-)
